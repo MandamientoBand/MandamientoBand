d9bcaaac943b1c743bff1d0043289b291efe98c4
MandamientoBand <93285708+MandamientoBand@users.noreply.github.com>
2021-10-27 13:02:36 -0600

    Archivos principales

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* ScrollReveal().reveal('.patrocinador-card', {delay: 1500}, { interval: 200 });
+ScrollReveal().reveal('.title'); */
+
+ScrollReveal().reveal('.title', { duration: 2000 });
+ScrollReveal().reveal('.patrocinador-card', { interval: 200, delay: 800 });
